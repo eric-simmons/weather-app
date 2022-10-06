ac450ebda84b6864ce90231a9e9a8d290f17e777
Eric Simmons <erictomlinsonsimmons@gmail.com>
2022-10-06 13:16:09 -0500

    fetch connected to search bar

--- script.js
+++ script.js
@@ -1,31 +1,50 @@
+
+
+
 //time
 const DateTime = luxon.DateTime;
 const now = DateTime.now();
 const date = now.toLocaleString(DateTime.DATETIME_MED)
 const currentHour = now.hour
-var city = 'chicago'
+
+
 //get elements from html
 var searchBar = document.getElementById('search')
 var container = document.getElementById('container')
+var form = document.querySelector('form')
+
+
+//event listener
+form.addEventListener('submit', searchCity )
+
 
 
 // var city = searchBar.value.trim();
 
-var APIKey = "fd21f9847f19d386e41cdfe3df89257d"
+
 // var city = "chicago"
 
 
 
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+//api key and query url
 
 
 
+function searchCity(event){
+    event.preventDefault()
+    
+    var city = searchBar.value
+    
+    
+    console.log(city)
+    
 
 
 
-
-//parse readable stream into JSON
-fetch(queryURL)
+    
+    var APIKey = "fd21f9847f19d386e41cdfe3df89257d"
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    fetch(queryURL)
 .then(function(response){
     return response.json()
 })
@@ -33,3 +52,9 @@ fetch(queryURL)
 .then(function(jsonData){
     console.log(jsonData)
 })
+}
+
+
+
+
+//parse readable stream into JSON
