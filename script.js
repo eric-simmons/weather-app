7fbe224ebc714bdba59c6bc7e91bb92d59cffdab
Eric Simmons <erictomlinsonsimmons@gmail.com>
2022-10-06 12:46:26 -0500

    fetch working

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
+//time
 const DateTime = luxon.DateTime;
 const now = DateTime.now();
 const date = now.toLocaleString(DateTime.DATETIME_MED)
 const currentHour = now.hour
+var city = 'chicago'
+//get elements from html
+var searchBar = document.getElementById('search')
+var container = document.getElementById('container')
+
+
+// var city = searchBar.value.trim();
+
 var APIKey = "fd21f9847f19d386e41cdfe3df89257d"
-//change to user input
-var city = "eau claire"
+// var city = "chicago"
+
 
 
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
 
+
+
+
+
+
+//parse readable stream into JSON
 fetch(queryURL)
+.then(function(response){
+    return response.json()
+})
+//retreive data
+.then(function(jsonData){
+    console.log(jsonData)
+})
